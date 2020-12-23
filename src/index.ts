f2adbf07c8827233a323553f7b92d64a5b000e59
himsch <55rsearchco@gmail.com>
2020-12-24 02:16:08 +0900

    Structure Check and Project Finish

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
+import { time } from "console";
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  // hash 생성 / 전역 메소드 생성 (객체생성하지 않아도 사용가능)
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+  // 구조 타입 체크 메소드
+  static validateStructure = (aBlock: Block): boolean =>
+    typeof aBlock.index === "number" &&
+    typeof aBlock.hash === "string" &&
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
+
   constructor(
     index: number,
     hash: string,
@@ -28,8 +49,73 @@ const genesisBlock: Block = new Block(
 );
 
 // 변수를 block 타입 Array 생성 후 그 안에 genesisBlock 넣는다.
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 
-console.log(blockchain);
+// Block 전체출력 메소드
+const getBlockchain = (): Block[] => blockchain;
+
+// Block 최근것만 출력 메소드
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+// timeStamp 생성 메소드 / Math.round(x) 반올림 메소드
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+// Block 생성 함수
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimeStamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimeStamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimeStamp
+  );
+  addBlock(newBlock);
+  return newBlock;
+};
+
+const getHashforBlock = (aBlock: Block): string =>
+  Block.calculateBlockHash(
+    aBlock.index,
+    aBlock.previousHash,
+    aBlock.timestamp,
+    aBlock.data
+  );
+
+// 구조 체크 함수
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+  if (!Block.validateStructure(candidateBlock)) {
+    return false;
+  } else if (previousBlock.index + 1 !== candidateBlock.index) {
+    return false;
+  } else if (previousBlock.hash !== candidateBlock.previousHash) {
+    return false;
+  } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+// Block 추가
+const addBlock = (candidateBlock: Block): void => {
+  if (isBlockValid(candidateBlock, getLatestBlock())) {
+    blockchain.push(candidateBlock);
+  }
+};
+
+createNewBlock("Second Blcok");
+createNewBlock("third Blcok");
+createNewBlock("fourth Blcok");
+
+console.log(getBlockchain());
 
 export {};

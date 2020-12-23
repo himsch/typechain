057c16f1796af82102f0140b6934469f5dfe8634
himsch <55rsearchco@gmail.com>
2020-12-23 17:23:29 +0900

    Chain Class Create

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,35 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const song = new Human("Song", 25, "female");
+const genesisBlock: Block = new Block(
+  0,
+  "202020202020202",
+  "",
+  "Hello",
+  123455
+);
 
-// 람다식 함수 =>
-const sayHI = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}. you are a ${person.gender}!`;
-};
+// 변수를 block 타입 Array 생성 후 그 안에 genesisBlock 넣는다.
+let blockchain: [Block] = [genesisBlock];
 
-console.log(sayHI(song));
+console.log(blockchain);
 
 export {};

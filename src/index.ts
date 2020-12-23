609a37c62425bb57b9fa80094a7dd1e2b5de3adf
himsch <55rsearchco@gmail.com>
2020-12-23 16:59:58 +0900

    TS class 1

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-// object
-const person = {
-  name: "song",
-  age: 25,
-  gender: "male",
-};
+const song = new Human("Song", 25, "female");
 
 // 람다식 함수 =>
 const sayHI = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}. you are a ${person.gender}!`;
 };
 
-console.log(sayHI(person));
+console.log(sayHI(song));
 
 export {};
